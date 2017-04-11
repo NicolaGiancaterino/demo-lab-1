6fce7409b0f79c8d46270c45444e4d4f30c5c281
Nicola Giancaterino <nicola.giancaterino@mail.polimi.it>
2017-04-11 14:39:05 +0200

    add signature

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,7 @@
 public class MyClass{
 
 	public static void main(STring[] args){
-		System.out.println("Hello!!");
+		String signature = "Nicola Giancaterino";
+		System.out.println("Hello!!" + signature);
 	}
 }
